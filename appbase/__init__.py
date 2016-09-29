4b4af20eb7fee44f3c98680650379a87bbf8fe88
Karl Bedrich <karl@bedrich.de>
2016-09-29 19:09:21 +0100

    update

--- appbase/__init__.py
+++ appbase/__init__.py
@@ -12,7 +12,7 @@ appbase is the foundation for a pyQt based application including:
 
 # import os as _os
 
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 __author__ = 'Karl Bedrich'
 __email__ = 'karl@bedrich.de'
 __url__ = 'https://github.com/radjkarl/appbase'
