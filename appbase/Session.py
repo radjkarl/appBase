9d84f860307a8903160a14a4024598b8c9b60567
Karl Bedrich <karl@bedrich.de>
2016-09-29 19:24:21 +0100

    update

--- appbase/Session.py
+++ appbase/Session.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-from future import standard_library
-standard_library.install_aliases()
+# from future import standard_library
+# standard_library.install_aliases()
 
 import numpy as np
 
